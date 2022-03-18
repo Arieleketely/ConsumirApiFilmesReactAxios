48a80d63fe95331c0c060bf7eff4124392cb0fc4
Arieleketely <arieleketely2012@hotmail.com>
2022-03-18 14:06:45 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import api from './Componentes/api';
+import React, { Component } from 'react';
 
-function App() {
+class App extends Component {
+
+  state = {
+    filmes: [],
+  }
+
+  async componentDidMount() {
+    const response = await api.get('star%20wars')
+ 
+    this.setState({ filmes: response.data });
+  }
+
+
+
+  render(){
+    const {filmes}=this.state;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      {console.log(filmes)}
+      {filmes.map(filme => (
+        <li key={filme.show.id}>
+          <h2>
+            <strong>
+              Título:
+              </strong>
+              </h2>
+              {filme.show.name}
+              {filme.show.url}
+         
+            
+     
+
+        </li>
+      ))}
+
+      
+      
+      
+      
+      
+      
+      
+      
+     
     </div>
   );
 }
-
+}
 export default App;
+
+
+
+
